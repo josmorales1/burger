ad1076c93c68c86e2de90430799f3f8620bfb1f2
josmorales1 <josgerardomorales1@aol.com>
2020-01-21 22:55:55 -0600

    finished w/ routes

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -10,4 +10,19 @@ router.get("/burgers", function (req, res) {
     burger.all(function (burgerData) {
         res.render("index", { burger_data: burgerData })
     })
-})
+});
+
+router.post("burger/create", function (req, res) {
+    burger.create(req.body, burger_name, function (result) {
+        res.redirect("/");
+    });
+});
+
+router.put("burgers/:id", function (req, res) {
+    burger.update(req.params.id, function (result) {
+        console.log(result);
+        res.sendStatus(200);
+    });
+});
+
+module.exports = router;
