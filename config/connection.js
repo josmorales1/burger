51cace9bb267a961bb2079c2a1462aeb557afe31
josmorales1 <josgerardomorales1@aol.com>
2020-01-21 22:12:43 -0600

    created the connections

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+});
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+// Export connection for our ORM to use.
+module.exports = connection;
