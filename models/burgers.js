a2145578bee1667ea64dc461c2e348ad570deaef
josmorales1 <josgerardomorales1@aol.com>
2020-01-21 22:24:17 -0600

    finshing the modules for burger

---
+++ models/burgers.js
@@ -0,0 +1,16 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        })
+    },
+    create: function (name, cb) {
+        orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
+    },
+    update: function (id, cb) {
+        var condition = "id=" + id;
+        orm.update("burgers", { devoured: true }, condition, cb);
+    }
+}
